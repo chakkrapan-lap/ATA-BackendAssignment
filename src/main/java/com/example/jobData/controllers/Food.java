eb008ba5198a1adf740fbd66928b95b9012a4278
chakkrapan-lap <141470129+chakkrapan-lap@users.noreply.github.com>
2025-07-10 00:09:55 +0700

    Update Food.java

--- src/main/java/com/example/jobData/controllers/Food.java
+++ src/main/java/com/example/jobData/controllers/Food.java
@@ -1,1 +1,23 @@
+package com.example.jobData.controllers;
 
+import com.example.jobData.services.JobService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api")
+public class Food {
+    private JobService jobService;
+    @GetMapping("/jobs")
+    public ResponseEntity<Object> getJobDataList(
+            @RequestParam(required = false) Map<String, String> request) {
+         System.out.println(41);
+        System.out.println(4901);
+        return ResponseEntity.status(HttpStatus.OK).body(jobService.getJobDateList(request));
+    }
+}
