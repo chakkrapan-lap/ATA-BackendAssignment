718759ef39028313e13d6b2314cc52968c197188
chakkrapan-lap <141470129+chakkrapan-lap@users.noreply.github.com>
2025-07-10 00:13:48 +0700

    Update Food.java

--- src/main/java/com/example/jobData/controllers/Food.java
+++ src/main/java/com/example/jobData/controllers/Food.java
@@ -17,7 +17,6 @@ public class Food {
     public ResponseEntity<Object> getJobDataList(
             @RequestParam(required = false) Map<String, String> request) {
          System.out.println(41);
-        System.out.println(4901);
         return ResponseEntity.status(HttpStatus.OK).body(jobService.getJobDateList(request));
     }
 }
